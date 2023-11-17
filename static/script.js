1a2e1dd3919c2aea73be027e8f66fb3b6002a2d2
Stephen Gruzin <stephengrzn@gmail.com>
2023-11-16 22:22:13 -0800

    Made the reconnect thing better

--- static/script.js
+++ static/script.js
@@ -1,12 +1,16 @@
 const WIN_URL = new URL(window.location.href);
 const SOCKET_ADDRESS = (WIN_URL.protocol.includes("https") ? "wss://" : "ws://") + WIN_URL.host + "/socket";
+const indicator = document.querySelector("#socketIndicator")
+let ws;
 
 function connect() {
-    let ws = new WebSocket(SOCKET_ADDRESS);
+    ws = new WebSocket(SOCKET_ADDRESS);
 
     function socketOpen() {
         // console.log(event);
         console.log("Socket connected.")
+        indicator.style.backgroundColor = "#07ff07";
+        indicator.children[0].innerText = "Connected";
         ws.send("test");
     }
 
@@ -18,6 +22,13 @@ function connect() {
     ws.onmessage = socketMessage;
 
     ws.onclose = function(e) {
+        if (indicator.children[0].innerText === "Connected") {
+            indicator.style.backgroundColor = "#d13a3a";
+            indicator.children[0].innerText = "Disconnected";
+        } else {
+            indicator.style.backgroundColor = "#ed8e1d";
+            indicator.children[0].innerText = "Reconnecting";
+        }
         console.log('Socket is closed. Reconnect will be attempted in 1 second.', e.reason);
         setTimeout(function() {
             connect();
@@ -25,6 +36,8 @@ function connect() {
     };
 
     ws.onerror = function(err) {
+        indicator.style.backgroundColor = "#d13a3a";
+        indicator.children[0].innerText = "Disconnected";
         console.error('Socket encountered error: ', err.message, 'Closing socket');
         ws.close();
     };
