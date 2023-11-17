041c22bede595182c33dfaab10724c87003cc11e
Stephen Gruzin <stephengrzn@gmail.com>
2023-11-16 21:51:58 -0800

    Added the ability to reconnect the socket when disconnected

--- static/script.js
+++ static/script.js
@@ -1,13 +1,33 @@
 const WIN_URL = new URL(window.location.href);
 const SOCKET_ADDRESS = (WIN_URL.protocol.includes("https") ? "wss://" : "ws://") + WIN_URL.host + "/socket";
 
-const websocket = new WebSocket(SOCKET_ADDRESS);
+function connect() {
+    let ws = new WebSocket(SOCKET_ADDRESS);
 
-websocket.onopen = (event) => {
-    // console.log(event);
-    websocket.send("test");
-}
+    function socketOpen() {
+        // console.log(event);
+        console.log("Socket connected.")
+        ws.send("test");
+    }
 
-websocket.onmessage = (event) => {
-    console.log(JSON.parse(event.data));
+    function socketMessage(event) {
+        console.log(JSON.parse(event.data));
+    }
+
+    ws.onopen = socketOpen;
+    ws.onmessage = socketMessage;
+
+    ws.onclose = function(e) {
+        console.log('Socket is closed. Reconnect will be attempted in 1 second.', e.reason);
+        setTimeout(function() {
+            connect();
+        }, 1000);
+    };
+
+    ws.onerror = function(err) {
+        console.error('Socket encountered error: ', err.message, 'Closing socket');
+        ws.close();
+    };
 }
+  
+connect();
