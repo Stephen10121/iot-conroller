0a73d091fab4e493c89e0fb544d560b5933e449c
Stephen Gruzin <stephengrzn@gmail.com>
2023-11-17 18:03:17 -0800

    Wasnt sending the actuall command to the broker.

--- function/custom.go
+++ function/custom.go
@@ -11,6 +11,7 @@ import (
 
 func RunCommand(command []string, params Command, conn *websocket.Conn, mqttClient mqtt.Client) error {
 	joinedCommand := strings.Join(command, " ")
+	actualCommand := params.MainCommand + " " + strings.Join(command[1:], " ")
 	if params.RelayArguments {
 		if len(command) < 2 {
 			err := conn.WriteMessage(websocket.TextMessage, []byte(`{"error":"no arguments given","command":"`+joinedCommand+`"}`))
@@ -22,8 +23,8 @@ func RunCommand(command []string, params Command, conn *websocket.Conn, mqttClie
 		}
 
 		err := conn.WriteMessage(websocket.TextMessage, []byte(`{"error":false,"command":"`+joinedCommand+`","data":"sent command"}`))
-		fmt.Println("Running command: " + command[0] + ". With args: " + command[1])
-		go messageqeue.Publish(mqttClient, joinedCommand)
+		fmt.Println("Running command: " + command[0] + ". With args: " + command[1] + ". Command sent to broker: " + actualCommand)
+		go messageqeue.Publish(mqttClient, actualCommand)
 
 		if err != nil {
 			return err
@@ -31,8 +32,8 @@ func RunCommand(command []string, params Command, conn *websocket.Conn, mqttClie
 		return nil
 	}
 	err := conn.WriteMessage(websocket.TextMessage, []byte(`{"error":false,"command":"`+joinedCommand+`","data":"sent command"}`))
-	fmt.Println("Running command: " + command[0])
-	go messageqeue.Publish(mqttClient, command[0])
+	fmt.Println("Running command: " + command[0] + ". Command sent to broker: " + actualCommand)
+	go messageqeue.Publish(mqttClient, actualCommand)
 
 	if err != nil {
 		return err
