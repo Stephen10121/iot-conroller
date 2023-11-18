eef5880cc34ac2f6049c295c2582602bef3bce8d
Stephen Gruzin <stephengrzn@gmail.com>
2023-11-18 14:15:10 -0800

    Making a playground for the iot controller

--- static/script.js
+++ static/script.js
@@ -1,21 +1,23 @@
 const WIN_URL = new URL(window.location.href);
 const SOCKET_ADDRESS = (WIN_URL.protocol.includes("https") ? "wss://" : "ws://") + WIN_URL.host + "/socket";
 const indicator = document.querySelector("#socketIndicator")
+const indicatorText = document.querySelector("#socketIndicatorText")
 let ws;
 
 function setStatus(status) {
+    if (!indicator || !indicatorText) return
     switch (status) {
         case "connected":
-            indicator.style.backgroundColor = "#07ff07";
-            indicator.children[0].innerText = "Connected";
+            indicator.style.backgroundColor = "#DBF4A7";
+            indicatorText.innerText = "Connected";
             break
         case "connecting":
             indicator.style.backgroundColor = "#ed8e1d";
-            indicator.children[0].innerText = "Reconnecting";
+            indicatorText.innerText = "Reconnecting";
             break
         default:
-            indicator.style.backgroundColor = "#d13a3a";
-            indicator.children[0].innerText = "Disconnected";
+            indicator.style.backgroundColor = "#db4b4b";
+            indicatorText.innerText = "Disconnected";
     }
 }
 
@@ -38,10 +40,10 @@ function connect() {
     ws.onmessage = socketMessage;
 
     ws.onclose = function(e) {
-        if (indicator.children[0].innerText === "Connected") {
+        if (indicatorText.innerText === "Connected") {
             setStatus("disconnected");
         } else {
-            setStatus("reconnecting");
+            setStatus("connecting");
         }
         console.log('Socket is closed. Reconnect will be attempted in 1 second.', e.reason);
         setTimeout(function() {
@@ -50,8 +52,9 @@ function connect() {
     };
 
     ws.onerror = function(err) {
-        indicator.style.backgroundColor = "#d13a3a";
-        indicator.children[0].innerText = "Disconnected";
+        // indicator.style.backgroundColor = "#d13a3a";
+        // indicatorText.innerText = "Disconnected";
+        setStatus();
         console.error('Socket encountered error: ', err.message, 'Closing socket');
         ws.close();
     };
